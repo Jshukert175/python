c4b98795c2a469e7309224b45275ce89bcd283cb
Jshukert175 <jess.shukert@gmail.com>
2025-11-20 13:28:36 -0600

    Update television.py
    Updated television.py

--- television.py
+++ television.py
@@ -1,21 +1,29 @@
 class Television:
     MIN_VOLUME = 0
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+    MAX_VOLUME = 2
 
     def __init__(self):
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
     def power(self):
         pass
     def mute(self):
         pass
     def channel_up(self):
-        pass
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
     def channel_down(self):
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
             else:
-                self.__channel = Television>MAX_CHANNEL
+                self.__channel = Television.MAX_CHANNEL
     
     def volume_up(self):
         if self.__status:
@@ -24,7 +32,10 @@ class Television:
                 self.__volume += 1
     
     def volume_down(self):
-        pass
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
 
     def __str__(self):
         if self.__muted:
